5e55bbbbc4998a6c95c4bf7fcf8b36c9eebd226e
Zhendong Tang <zhendongtt@gmail.com>
2021-12-30 12:53:05 -0500

    5-3 Adding Activity interface

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
+import {Activity} from "../models/activity";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
+    axios.get<Activity[]>("http://localhost:5000/api/activities").then((response) => {
       console.log(response);
       setActivities(response.data);
     });
