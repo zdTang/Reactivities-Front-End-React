673d2d346de631f65586d9dd2157354ad8c51fd8
Zhendong Tang <zhendongtt@gmail.com>
2021-12-31 14:50:23 -0500

    3 - Axios types

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -1,19 +1,21 @@
 import axios, {AxiosResponse} from "axios" // AxiosResponse is an interface
+import { Activity } from "../models/activity";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
-const responseBody=(response:AxiosResponse)=>response.data;
+const responseBody=<T>(response:AxiosResponse<T>)=>response.data;
 
 // Review Promise:
 // the Return value from the the first method will post to the first method in the "then"
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),   // get is a method.
-  post:(url:string,body:{})=>axios.post(url,body).then(responseBody),
-  put: (url:string,body:{})=>axios.put(url,body).then(responseBody),
-  del:(url:string)=>axios.delete(url).then(responseBody)
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody), // get is a method.
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activities={
-    list:()=>requests.get('/activities') // notice the BaseURL
+    list:()=>requests.get<Activity[]>('/activities') // notice the BaseURL
 }
 
 const agent={
