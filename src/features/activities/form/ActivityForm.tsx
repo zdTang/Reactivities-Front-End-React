ca52ec0c54d13230720f16f9f6f56a650d1304d2
Zhendong Tang <zhendongtt@gmail.com>
2021-12-31 10:33:11 -0500

    5-12 - Editing an activity and form basics in React

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -1,22 +1,77 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
 }
+// use Alias here, as we want use the "activity" in useState
+// So that we can name the following "activity" as another name to avoid duplication
+// activity:selectedActivity, Here, it is not a type or option value, it is an Alias
+const ActivityForm = ({ activity: selectedActivity, closeForm }: Props) => {
+  const initialState = selectedActivity ?? {
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  };
+  const [activity, setActivity] = useState(initialState);
 
-const ActivityForm = ({ activity, closeForm }: Props) => {
+  function handleSubmit() {
+    console.log(activity);
+  }
+
+  //this is a new approach to bind Form with an Object
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target; // "name" and "value" are properties of "event.target"
+    setActivity({ ...activity, [name]: value }); // use new property to cover old value
+  }
+
   return (
     // "clearing" to clear previous float content?
     <Segment clearing>
-      <Form>
-        <Form.Input placeholder="Title" />
-        <Form.TextArea placeholder="Description" />
-        <Form.Input placeholder="Category" />
-        <Form.Input placeholder="Date" />
-        <Form.Input placeholder="City" />
-        <Form.Input placeholder="Venue" />
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          value={activity.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          value={activity.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          value={activity.category}
+          name="category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          value={activity.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="City"
+          value={activity.city}
+          name="city"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          value={activity.venue}
+          name="venue"
+          onChange={handleInputChange}
+        />
         <Button floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={closeForm}
