9d0050ab38261412b727d371953800f41d065979
Zhendong Tang <zhendongtt@gmail.com>
2021-12-31 22:22:42 -0500

    Add an Action to update the state

--- src/app/stores/activityStore.ts
+++ src/app/stores/activityStore.ts
@@ -1,10 +1,13 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 
 // this is a class, a component ??
 export default class ActivityStore {
-  title = "Hello from MobX";      // this is a state
+  title = "Hello from MobX"; // this is a state
+  setTitle = () => {
+    this.title = this.title + "!";
+  };
 
   constructor() {
-    makeObservable(this, { title: observable }); // make this state abservable
+    makeObservable(this, { title: observable, setTitle: action }); // make this state abservable
   }
 }
