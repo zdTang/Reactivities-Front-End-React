2ae6b883d88d80562f027c61741b69fc0eab74c9
Zhendong Tang <zhendongtt@gmail.com>
2022-01-01 16:38:12 -0500

    8-2 Install React router

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
-import {store, StoreContext} from "./app/stores/store"
+import { store, StoreContext } from "./app/stores/store";
+import { BrowserRouter } from "react-router-dom";
 
 // StoreContext = createContext(store);
 // store = {activityStore: new ActivityStore()}
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>, document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreContext.Provider>,
+  document.getElementById("root")
 );
