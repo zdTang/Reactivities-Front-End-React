7fb83ce07a177fb10f4c4afe0c3188c58c6f4b28
Zhendong Tang <zhendongtt@gmail.com>
2022-01-01 17:34:46 -0500

    Create several routes

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import LoadingComponent from "./LoadingComponent";
 import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "../../features/activities/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 function App() {
   const { activityStore } = useStore();
-  const {loadingInitial}=activityStore;
+  const { loadingInitial } = activityStore;
 
   useEffect(() => {
     activityStore.loadActivities();
@@ -20,7 +23,9 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/activities" component={ActivityDashboard} />
+        <Route exact path="/createActivity" component={ActivityForm} />
       </Container>
     </>
   );
---
+++ src/features/activities/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+const HomePage = () => {
+  return (
+    <Container style={{ marginTop: "7em" }}>
+      <h1>Home page</h1>
+    </Container>
+  );
+};
+
+export default HomePage;
