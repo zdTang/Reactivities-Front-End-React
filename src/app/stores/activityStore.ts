72e8354d75e821ae97e551656db1e17ec676e463
Zhendong Tang <zhendongtt@gmail.com>
2022-01-01 07:28:52 -0500

    7-5 - Refactoring the app to use MobX

--- src/app/stores/activityStore.ts
+++ src/app/stores/activityStore.ts
@@ -18,21 +18,21 @@ export default class ActivityStore {
   selectedActivity: Activity | null = null;
   editMode = false;
 
-  loadActivities = () => {
+  loadActivities = async () => {
     // Reference the "agent" module
     // here the "response" is the "response.data"
     this.loadingInitial = true;
-    agent.Activities.list().then((response) => {
-      // the date string from ASP.NET core API is very long
-      // use the following code to just get the YEAR-MONTH-DAY
-      // and ignore the Time
+    try {
+      var response = await agent.Activities.list();
       response.forEach((activity: Activity) => {
         activity.date = activity.date.split("T")[0];
         this.activities.push(activity);
       });
       this.loadingInitial = false;
       console.log("App-useEffect-Get-Activities-Axios: ", response);
-    });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   constructor() {
