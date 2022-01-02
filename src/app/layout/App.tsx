09e06eaa6befaa8aa60062b02e2423fc2b94c153
Zhendong Tang <zhendongtt@gmail.com>
2022-01-01 19:41:27 -0500

    8-3 - Adding routes.mp4

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from "../../features/activities/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 
 function App() {
-  const { activityStore } = useStore();
-  const { loadingInitial } = activityStore;
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (loadingInitial) return <LoadingComponent content="Loading app" />;
-  // SelectedActivity is the Activity user chose from the ActivityList
   return (
     <>
       <NavBar />
--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 import {useStore} from "../../../app/stores/store"
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 
 const ActivityDashboard = () => {
-  const {activityStore}=useStore();
-  const { selectedActivity, editMode } = activityStore;
+  const { activityStore } = useStore();
+  const { selectedActivity, editMode, loadingInitial } = activityStore;
+  
+  // study this useEffect
+  // any change in activityStore, will reload 
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+
+  if (loadingInitial) return <LoadingComponent content="Loading app" />;
+  // SelectedActivity is the Activity user chose from the ActivityList
   return (
     <div>
       <Grid>
         <Grid.Column width={10}>
-          <ActivityList/>
+          <ActivityList />
         </Grid.Column>
         <Grid.Column width={6}>
-          {selectedActivity && !editMode && (
-            <ActivityDetails/>
-          )}
+          {selectedActivity && !editMode && <ActivityDetails />}
           {editMode && <ActivityForm />}
         </Grid.Column>
       </Grid>
     </div>
   );
-};
+};;
 
 export default observer(ActivityDashboard);
