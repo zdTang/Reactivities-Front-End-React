6ede35211275f18b94b7f092a439d50bb6a90106
Zhendong Tang <zhendongtt@gmail.com>
2022-01-03 21:22:15 -0500

    End of 9-styling

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { observer } from "mobx-react-lite";
+import HomePage from "../../features/home/HomePage";
+import { Route, useLocation } from "react-router-dom";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 function App() {
-  const [activities, setActivities] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      console.log(response);
-      setActivities(response.data);
-    });
-  }, []);
-
+  const location = useLocation();
   return (
-    <div>
-      <Header as="h2" icon="users" content="Reactivities" />
-
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={["/createActivity", "/manage/:id"]}
+                component={ActivityForm}
+              />
+            </Container>
+          </>
+        )}
+      />
+    </>
   );
 }
 
-export default App;
+export default observer(App);
