91fa88fb50378446adb17ce2b73eee40edec2e6c
Zhendong Tang <zhendongtt@gmail.com>
2022-01-04 09:52:45 -0500

    create NotFound page and add route to activate it once necessary

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -3,17 +3,18 @@ import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
 import HomePage from "../../features/home/HomePage";
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import { ToastContainer } from "react-toastify";
+import NotFound from "../../features/errors/NotFound";
 
 function App() {
   const location = useLocation();
   return (
     <>
-    <ToastContainer position="bottom-right" hideProgressBar/>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
@@ -21,14 +22,18 @@ function App() {
           <>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-              />
-              <Route path="/errors" component={TestErrors}/>
+              {/* switch can make the components within to be activated only one time. */}
+              <Switch> 
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route path="/errors" component={TestErrors} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
